58a2aade0d1225b6a1c1902951900a32cc66447e
denomycor <afonsoresteves@hotmail.com>
2021-03-22 13:31:23 +0000

    Started working on memory.c

--- include/memory-private.h
+++ include/memory-private.h
@@ -1,15 +1,36 @@
 #ifndef MEMORY_PRIVATE_H_GUARD
 #define MEMORY_PRIVATE_H_GUARD
 
+#include<stdlib.h>
 
-//estrutura que representa um buffer circular, completar pelos alunos
+//Defines the buffer size for buffers
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+//Defenition of operation struct, so this file can see it
+struct operation; //memory.h
+
+//Defines the elements to be saved in the buffers
+typedef buffer_elem operation;
+
+
 struct circular_buffer { 	
-
+    
 };
 
-//estrutura que representa um buffer de acesso aleatório, completar pelos alunos
+
 struct rnd_access_buffer { 		
+    int flags[BUFFER_SIZE];
+    buffer_elem* elems[BUFFER_SIZE];
 
 };
+
+
+void rnd_access_buffer_clear(struct rnd_access_buffer* o){
+    memset(o->flags, 0, sizeof(int)*BUFFER_SIZE);
+}
+
+
 
 #endif
