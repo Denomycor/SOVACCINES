99a247e8275552a242a817fb58d5513b793983da
migas342 <56080093+migas342@users.noreply.github.com>
2021-03-27 21:46:34 +0000

    Update process.c

--- src/process.c
+++ src/process.c
@@ -1,52 +1,39 @@
-#include "process.h"
-#include "client.c"
-#include "proxy.c"
-#include "server.c"
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+#include <client.h>
+#include <server.h>
+#include <proxy.h>
 
-int launch_process(int process_id, int process_code, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems) {
-    int pid; 
-    if((pid = fork())== -1) {
-        perror("erro");
-        exit(-1);
+int launch_process(int process_id, int process_code, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
+ int pid;
+ if ((pid = fork()) == -1){
+     perror("An error has occured");
+     exit(1);
+ }
+ if (pid == 0){     
+     int x; 
+    if (process_code == 0) {
+       x = execute_client(process_id,buffers,data,sems);
+       exit(x);
     }
-    if(pid == 0) {
-        switch (process_code)
-        {
-        case '0':
-            data.n_clients++;
-            data.*client_pids = &process_id;
-            exit(execute_client(data.n_clients,buffers,data,sems));
-            break;
-        case '1':
-            data.n_proxies++;
-            data.*proxy_pids = &process_id;
-            exit(execute_proxy(data.n_proxies,buffers,data,sems));
-            break;
-        case '2':
-            data.n_servers++;
-            data.*server_pids = &process_id;
-            exit(execute_server(data.n_servers,buffers,data,sems));
-            break;
-        default:
-            perror("Process_code is wrong");
-        }
-    }
-    else {
-        exit(getpid());
+    if (process_code == 1) {
+       x = execute_proxy(process_id,buffers,data,sems);
+       exit(x);
     }
 
+    if (process_code == 2) {
+       x = execute_server(process_id,buffers,data,sems);
+       exit(x);
+    }
+ } else {
+     return pid;
+ }
 }
 
-int wait_process(int process_id) {
-    int *status_process;
-    waitpid(process_id,*status_process,0);
-    if(WIFEXITED(status_process)) {
-        return status_process;
-    }
-    else {
-        perror("Processo acabou de maneira inesperada");
-        exit(-1);
-    }
+int wait_process(int process_id){
+ int* status;   
+ waitpid(process_id,&status,0);
+ if (WIFEXITED(status)){
+     return WEXITSTATUS(status);
+ }
 }
+
