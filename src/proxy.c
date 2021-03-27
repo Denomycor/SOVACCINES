7b97d4327c0cae31470ac960f6f1abd0a84d6561
migas342 <56080093+migas342@users.noreply.github.com>
2021-03-27 03:58:36 +0000

    changed '=!' to '!='  e completei receive_operation

--- src/proxy.c
+++ src/proxy.c
@@ -7,7 +7,7 @@ int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct ma
     struct operation* op;
     proxy_receive_operation(op,buffers,data,sems);
 
-    if((op->id) =! -1 && *(data->terminate) == 0){
+    if((op->id) != -1 && *(data->terminate) == 0){
         proxy_process_operation(op,proxy_id,data->proxy_stats);
         proxy_forward_operation(op,buffers,data,sems);
     }
@@ -20,6 +20,11 @@ int execute_proxy(int proxy_id, struct communication_buffers* buffers, struct ma
 
 void proxy_receive_operation(struct operation* op, struct communication_buffers* buffers, struct main_data* data, struct semaphores* sems){
  consume_begin(sems->cli_prx);
+ 
+ if(data->terminate == 1){
+     return;
+ }
+ 
  read_circular_buffer(buffers->cli_prx,data->buffers_size,op);
  consume_end(sems->cli_prx);
 }
