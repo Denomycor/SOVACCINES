135b495e4ca526bc2976668a9d6b502357876aef
migas342 <56080093+migas342@users.noreply.github.com>
2021-04-28 16:15:57 +0100

    Create sosignal.c

---
+++ src/sosignal.c
@@ -0,0 +1,19 @@
+/**
+Grupo054
+Afonso Esteves, fc54394
+Diogo Fernandes, fc54458
+Miguel Santos, fc54461
+*/
+
+#include <signal.h>
+#include "../include/main.h"
+
+void sighandler(int signum,struct main_data* data, struct communication_buffers* buffers, struct semaphores* sems) {
+    printf("Caught signal %d , terminating program", signum);
+    stop_execution(struct main_data* data, struct communication_buffers* buffers, struct semaphores* sems);
+}
+
+void sinal_horario(int alarm_time) {
+    printf("write statistics from statistics_filename");
+    alarm(alarm_time);                                      //keeps the alarm running with already given value
+}
