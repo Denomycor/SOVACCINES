c303c0066d9390c6ccdfeee6e2c4a8af75a72683
diogo0501 <71658555+diogo0501@users.noreply.github.com>
2021-04-30 03:04:49 +0100

    Implemented 5 methods

--- src/stats.c
+++ src/stats.c
@@ -1,20 +1,48 @@
-
 #include "stats.h"
+#include <stdio.h>
 
-const char* getFileName (FILE *stream) {
-    char fileName[30];
-    int cont = 0;
-    while(cont > 7) {
-        fscanf(stream,"%s",fileName);
-        cont++;
-    }
-    return fileName;
+void writeStatisticsFile(FILE *stream,struct main_data* data,int i){
+    writeProcessStats(stream,data,i);
+    writeNumberOP(stream,data->op[i],i);
+    writeStatus(stream,data->op[i],i);
+    writeIds(stream,data->op[i],i);
+    writeLocalsTime(stream,data->op[i],i);
+
 }
-void writeProcessStats(FILE *stream,struct main_data* data) {
-    char *client;
-    char *proxy; 
-    char *server;
-    asprintf(&client, "Client %d received %d requests", char1, char2);
-    asprintf(&proxy, "Proxy %d received %d requests", char1, char2);
-    asprintf(&server, "Server %d received %d requests", char1, char2);
+
+void writeProcessStats(FILE *stream,struct main_data* data, int i) {
+    fputs("Process Statistics:\n",stream);
+    char* client;
+    char* proxy;
+    char* server;
+    asprintf(&client,"Client %d received %d requests\n",data->client_pids[i],data->client_stats[i]);
+    asprintf(&proxy,"Client %d received %d requests\n",data->proxy_pids[i],data->proxy_stats[i]);
+    asprintf(&server,"Client %d received %d requests\n",data->server_pids[i],data->server_stats[i]);
+    fputs(&client,stream);
+    fputs(&proxy,stream);
+    fputs(&server,stream);
+}
+
+void writeNumberOp(FILE *stream,struct operation* op,int i) {
+    char* opStr;
+    asprintf(&opStr,"OP:%d\n",op[i]->id);
+    fputs(&opStr,stream);
+}
+
+void writeStatus(FILE *stream,struct operation* op,int i) {
+    char* opStr;
+    asprintf(&opStr,"OP:%d\n",op[i]->status);
+    fputs(&opStr,stream);
+}
+
+void writeIds(FILE *stream,struct operation* op,int i) {
+    char* clientId;
+    char* proxyId;
+    char* serverId;
+    asprintf(&clientId,"Client_id:%d\n",op[i]->client);
+    asprintf(&proxyId,"Client_id:%d\n",op[i]->proxy);
+    asprintf(&serverId,"Client_id:%d\n",op[i]->server);
+    fputs(&clientId,stream);
+    fputs(&proxyId,stream);
+    fputs(&serverId,stream);
 }
