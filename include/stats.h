eabfa8cc875d434edf9daac211d2d81a9f06a47d
diogo0501 <71658555+diogo0501@users.noreply.github.com>
2021-05-01 18:45:44 +0100

    introduce new mthod header

--- include/stats.h
+++ include/stats.h
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include "main.h"
 
-void writeStatisticsFile(FILE *stream,struct main_data* data,int i);
+void writeStatisticsFile(FILE *stream,struct main_data* data);
 
-void writeProcessStats(FILE *stream,struct main_data* data,int i);
+void writeProcessStats(FILE *stream,struct main_data* data);
+
+void writeOpStatistics(FILE *stream,struct operation* op);
 
 void writeNumberOp(FILE *stream,struct operation* op,int i);
 
@@ -13,5 +15,3 @@ void writeStatus(FILE *stream,struct operation* op,int i);
 void writeIds(FILE *stream,struct operation* op, int i);
 
 void writeLocalsTime(FILE *stream,struct operation* op,int i);
-
-/*void writeTotalTime(FILE *stream);*/
