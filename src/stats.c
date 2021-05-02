bb082a8de98bc1740bf3cd9c247a2445e30f0efe
diogo0501 <71658555+diogo0501@users.noreply.github.com>
2021-05-02 01:28:15 +0100

    sprintf corrected

--- src/stats.c
+++ src/stats.c
@@ -12,7 +12,7 @@ void writeProcessStats(FILE *stream,struct main_data* data) {
     fputs("Process Statistics:\n",stream);
     for(int i = 0; i < data ->n_clients;i++) {
         char* client;
-        asprintf(&client,"Client %d received %d requests\n",
+        sprintf(client,"Client %d received %d requests\n",
                 data->client_pids[i],  
                 data->client_stats[i]);
         fputs(client,stream);
@@ -20,13 +20,13 @@ void writeProcessStats(FILE *stream,struct main_data* data) {
     
     for(int i = 0; i < data ->n_proxies;i++) {
         char* proxy;
-        asprintf(&proxy,"Proxy %d received %d requests\n",data->proxy_pids[i],data->proxy_stats[i]);
+        sprintf(proxy,"Proxy %d received %d requests\n",data->proxy_pids[i],data->proxy_stats[i]);
         fputs(proxy,stream);
     }
 
     for(int i = 0; i < data ->n_servers;i++) {
         char* server;
-        asprintf(&server,"Server %d received %d requests\n",data->server_pids[i],data->server_stats[i]);
+        sprintf(server,"Server %d received %d requests\n",data->server_pids[i],data->server_stats[i]);
         fputs(server,stream);
     }
 }
@@ -45,13 +45,13 @@ void writeOpStatistics(FILE *stream,struct operation* op,struct main_data* data)
 }
 void writeNumberOp(FILE *stream,struct operation* op,int i) {
     char* opStr;
-    asprintf(&opStr,"OP:%d\n",op[i].id);
+    sprintf(opStr,"OP:%d\n",op[i].id);
     fputs(opStr,stream);
 }
 
 void writeStatus(FILE *stream,struct operation* op,int i) {
     char* opStr;
-    asprintf(&opStr,"OP:%d\n",op[i].status);
+    sprintf(opStr,"OP:%d\n",op[i].status);
     fputs(opStr,stream);
 }
 
@@ -59,9 +59,9 @@ void writeIds(FILE *stream,struct operation* op,int i) {
     char* clientId;
     char* proxyId;
     char* serverId;
-    asprintf(&clientId,"Client_id:%d\n",op[i].client);
-    asprintf(&proxyId,"Client_id:%d\n",op[i].proxy);
-    asprintf(&serverId,"Client_id:%d\n",op[i].server);
+    sprintf(clientId,"Client_id:%d\n",op[i].client);
+    sprintf(proxyId,"Client_id:%d\n",op[i].proxy);
+    sprintf(serverId,"Client_id:%d\n",op[i].server);
     fputs(clientId,stream);
     fputs(proxyId,stream);
     fputs(serverId,stream);
@@ -74,12 +74,12 @@ void writeLocalsTime(FILE *stream,struct operation* op,int i) {
     char* serverTime; 
     char* ended;
     char* totalTime;
-    asprintf(&createdTime,"Created:%s\n",ctime((time_t*)op[i].start_time.tv_sec));
-    asprintf(&clientTime,"Client_time:%s\n",ctime((time_t*)op[i].client_time.tv_sec));
-    asprintf(&proxyTime,"Porxy_time:%s\n",ctime((time_t*)op[i].proxy_time.tv_sec));
-    asprintf(&serverTime,"Server_time:%s\n",ctime((time_t*)op[i].server_time.tv_sec));
-    asprintf(&ended,"Ended:%s\n",ctime((time_t*)op[i].end_time.tv_sec));
-    asprintf(&totalTime,"Total Time:%s\n",ctime((time_t*)op[i].end_time.tv_sec - op[i].start_time.tv_sec));
+    sprintf(createdTime,"Created:%s\n",ctime((time_t*)op[i].start_time.tv_sec));
+    sprintf(clientTime,"Client_time:%s\n",ctime((time_t*)op[i].client_time.tv_sec));
+    sprintf(proxyTime,"Porxy_time:%s\n",ctime((time_t*)op[i].proxy_time.tv_sec));
+    sprintf(serverTime,"Server_time:%s\n",ctime((time_t*)op[i].server_time.tv_sec));
+    sprintf(ended,"Ended:%s\n",ctime((time_t*)op[i].end_time.tv_sec));
+    sprintf(totalTime,"Total Time:%s\n",ctime((time_t*)op[i].end_time.tv_sec - op[i].start_time.tv_sec));
     fputs(createdTime,stream);
     fputs(clientTime,stream);
     fputs(proxyTime,stream);
